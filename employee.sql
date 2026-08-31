41b0dc081577df42e4e8ad3f2468715f1df4f524
RACHITHA K <rachitha102007@gmail.com>
2026-08-31 21:48:06 +0530

    added employee SLQ

---
+++ employee.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employees(
+	id INT,
+	name VARCHAR(50),
+	department VARCHAR(30),
+	salary INT
+);
+INSERT INTO employees VALUES
+(1,'Rachitha','IT',30000),
+(2,'priya','HR',25000),
+(3,'Arun','IT',40000),
+(4,'sneha','sales',28000);
+SELECT * FROM employees;
