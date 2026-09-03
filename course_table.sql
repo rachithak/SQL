2daf7783ac61750311bce3aef076f775dbb0277c
RACHITHA K <rachitha102007@gmail.com>
2026-09-03 19:25:56 +0530

    added

---
+++ course_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE course(
+	id INT,
+	name VARCHAR(50),
+    roll INT
+);
+INSERT INTO course VALUES
+(1,'cse',23),
+(2,'ece',32),
+(3,'eee',12),
+(4,'mech',21),
+(5,'cse',13);
+SELECT * FROM course;
